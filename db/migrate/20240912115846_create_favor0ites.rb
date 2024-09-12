1477b285134447b0865637659523f3fe5cd92476
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-12 13:27:45 +0000

    [add]each tables

---
+++ db/migrate/20240912115846_create_favor0ites.rb
@@ -0,0 +1,11 @@
+class CreateFavor0ites < ActiveRecord::Migration[6.1]
+  def change
+    create_table :favor0ites do |t|
+      t.references :grous, null: false, foreign_key: true
+      t.references :posts, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,95 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2024_09_12_120854) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "posts_id", null: false
+    t.integer "users_id", null: false
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "grous_id", null: false
+    t.integer "posts_id", null: false
+    t.integer "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["grous_id"], name: "index_favorites_on_grous_id"
+    t.index ["posts_id"], name: "index_favorites_on_posts_id"
+    t.index ["users_id"], name: "index_favorites_on_users_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "users_id", null: false
+    t.integer "groups_id", null: false
+    t.string "title"
+    t.text "first_memo"
+    t.text "second_memo"
+    t.text "third_memo"
+    t.boolean "post_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["groups_id"], name: "index_posts_on_groups_id"
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
+
+  create_table "relations", force: :cascade do |t|
+    t.integer "follow_id"
+    t.integer "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.boolean "user_status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "favorites", "grous", column: "grous_id"
+  add_foreign_key "favorites", "posts", column: "posts_id"
+  add_foreign_key "favorites", "users", column: "users_id"
+  add_foreign_key "posts", "groups", column: "groups_id"
+  add_foreign_key "posts", "users", column: "users_id"
+end

6eb89978af75ef0592386a258d98b5af00a401cc
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-13 05:48:13 +0000

    [add]each routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  namespace :users do
+    get 'mypage', as: '/mypage'
+    # 5行目のルーティングから
+    get 'users/edit'
+    get 'users/show'
+    get 'users/update'
+    get 'users/unsubscribe'
+    get 'users/destroy'
+
+    get 'homes/top'
+    get 'homes/about'
+  end
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -7,6 +20,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
 
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]

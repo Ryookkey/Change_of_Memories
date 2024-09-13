15454948f2a9fc69cbee2ee0f693b6a33e03a0c7
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-13 21:29:06 +0000

    [add]each routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,30 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'mypage', as: '/mypage'
-    # 5行目のルーティングから
-    get 'users/edit'
-    get 'users/show'
-    get 'users/update'
-    get 'users/unsubscribe'
-    get 'users/destroy'
 
-    get 'homes/top'
-    get 'homes/about'
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+  get 'mypage' => 'public/users#mypage'
+  
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :posts
+  resources :favorite, only: [:index, :create, :destroy]
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
   end
 
+  namespace :users do
+    get 'unsubscribe'
+  end
+  
+  namespace :admin do
+    get '' => 'homes#top'
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :edit, :update, :destroy]
+  end
+  
+  
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -20,13 +33,4 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-
-  root to: 'public/homes#top'
-  get 'about' => 'public/homes#about'
-
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
-      get "followings" => "relationships#followings", as: "followings"
-      get "followers" => "relationships#followers", as: "followers"
-  end
 end

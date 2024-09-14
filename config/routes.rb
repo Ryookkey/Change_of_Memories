15f780ea698feb5f5854004b8804f632b6c7c219
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-14 15:53:22 +0000

    [add]top_page,about

--- config/routes.rb
+++ config/routes.rb
@@ -2,29 +2,34 @@ Rails.application.routes.draw do
 
   root to: 'public/homes#top'
   get 'about' => 'public/homes#about'
-  get 'mypage' => 'public/users#mypage'
-  
-  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
-  resources :posts
-  resources :favorite, only: [:index, :create, :destroy]
-  
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
+  namespace :public do
+    get 'mypage', to: 'users#show'
+
+    resources :users, only: [:index, :show, :edit, :update, :destroy] do
+      resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
+    end
+
+    get 'users/unsubscribe', to: 'public/users#unsubscribe'
   end
 
-  namespace :users do
-    get 'unsubscribe'
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  
+
+
+  resources :posts
+  resources :favorites, only: [:index, :create, :destroy]
+
+
   namespace :admin do
     get '' => 'homes#top'
     resources :users, only: [:index, :show, :edit, :update]
     resources :posts, only: [:index, :show, :edit, :update, :destroy]
   end
-  
-  
+
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'

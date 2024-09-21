66174278c13712be21f16fa78b8da659d55ba32d
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-21 10:36:17 +0000

    [fix]favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,8 +2,13 @@ class Public::FavoritesController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
+    favorite = current_user.favorites.new(post_id: post.id, group_id: post.group_id)
+
+    if favorite.save
+      flash[:notice] = "いいねを追加しました"
+    else
+      flash[:alert] = favorite.errors.full_messages.to_sentence
+    end
     redirect_to public_post_path(post)
   end
 
@@ -13,8 +18,10 @@ class Public::FavoritesController < ApplicationController
 
     if favorite.present?
       favorite.destroy
+      flash[:notice] = "いいねを削除しました"
+    else
+      flash[:alert] = "いいねの削除に失敗しました"
     end
-
     redirect_to public_post_path(post)
   end
 

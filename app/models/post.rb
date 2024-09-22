1977aba88736d4797749efa89a10740339e3b8df
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-22 06:31:57 +0000

    [add]search

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,23 @@ class Post < ApplicationRecord
   belongs_to :group
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("first_memo LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("first_memo LIKE?", "#{word}")
+    elsif search == "backward_match"
+      @post = Post.where("first_memo LIKE?", "#{word}")
+    elsif search == "partia_match"
+      @post = Post.where("first_memo LIKE?", "#{word}")
+    else
+      @post = Post.all
+    end
+  end
+
 end

f970f7dad5d2d4feb937180a372d8f0d02a2fc1c
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-09-27 22:00:08 +0000

    [add]コメント一覧画面と削除機能、投稿詳細画面

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,14 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
+  def index
+    @comments = Comment.includes(:post, :user).page(params[:page]).per(10)
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
     flash[:notice] = "コメントを削除しました。"
-    redirect_back(fallback_location: admin_post_path(@comment.post))
+    redirect_back(fallback_location: admin_comments_path)
   end
 end

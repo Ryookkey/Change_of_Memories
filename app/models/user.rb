835f479c0ddda289b597c387e736725ac5ed0853
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-10-24 07:46:57 +0000

    deviseメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 氏名のバリデーション
+  validates :name, presence: true
+
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
